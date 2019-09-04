a684c1d8ba1ea216253cc0be39324cf717efa0cc
Ryan Collingham <ryanc118@gmail.com>
2019-09-04 15:04:39 +0100

    Remove duplicates without using buffer

--- linked_lists.go
+++ linked_lists.go
@@ -1,5 +1,10 @@
 package ctci
 
+import (
+    "fmt"
+    "strings"
+)
+
 type Node struct {
 	next *Node
 	data int
@@ -25,4 +30,41 @@ func (list *Node) RemoveDups() {
 			currNode = currNode.next
 		}
 	}
+}
+
+// Remove duplicates from a linked-list without using any additional buffer.
+func (list *Node) RemoveDupsNoBuf() {
+    for currNode := list; currNode != nil; currNode = currNode.next {
+        currNode.RemoveAllAfter(currNode.data)
+    }
+}
+
+// Remove all nodes after the current node that contain a particular value.
+func (list *Node) RemoveAllAfter(removeData int) {
+    if list == nil {
+        return
+    }
+
+    for currNode := list; currNode.next != nil; {
+        if currNode.next.data == removeData {
+            // Delete the next node.
+            currNode.next = currNode.next.next
+        } else {
+            currNode = currNode.next
+        }
+    }
+}
+
+// Format all nodes in the list as a string.
+func (list *Node) String() string {
+    var b strings.Builder
+
+    for currNode := list; currNode != nil; currNode = currNode.next {
+        fmt.Fprintf(&b, "%d", currNode.data)
+        if currNode.next != nil {
+            b.WriteString(" -> ")
+        }
+    }
+
+    return b.String()
 }
--- linked_lists_test.go
+++ linked_lists_test.go
@@ -27,6 +27,7 @@ func TestRemoveDups(t *testing.T) {
 	}
 
 	list.RemoveDups()
+    t.Log(list.String())
 
 	listNode := &list
 	expectedNode := &expected
@@ -42,6 +43,57 @@ func TestRemoveDups(t *testing.T) {
 		t.Error()
 	}
 
-	var empty Node
-	empty.RemoveDups()
+    var empty *Node
+    empty.RemoveDups()
+    if empty != nil {
+        t.Error()
+    }
+}
+
+// Test the RemoveDupsNoBuf method.
+func TestRemoveDupsNoBuf(t *testing.T) {
+	list := Node{
+		data: 1, next: &Node{
+			data: 1, next: &Node{
+				data: 2, next: &Node{
+					data: 2, next: &Node{
+						data: 3, next: &Node{
+							data: 3, next: nil,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	expected := Node{
+		data: 1, next: &Node{
+			data: 2, next: &Node{
+				data: 3, next: nil,
+			},
+		},
+	}
+
+	list.RemoveDupsNoBuf()
+    t.Log(list.String())
+
+	listNode := &list
+	expectedNode := &expected
+	for listNode != nil && expectedNode != nil {
+		if listNode.data != expectedNode.data {
+			t.Error()
+		}
+		listNode = listNode.next
+		expectedNode = expectedNode.next
+	}
+
+	if listNode != nil || expectedNode != nil {
+		t.Error()
+	}
+
+    var empty *Node
+    empty.RemoveDupsNoBuf()
+    if empty != nil {
+        t.Error()
+    }
 }
