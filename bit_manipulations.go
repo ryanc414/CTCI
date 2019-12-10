b3406382d98984d9a770c2ef5d317cf43f42e227
Ryan Collingham <ryanc118@gmail.com>
2019-12-10 11:53:27 +0000

    Convert floats into binary string representation

--- bit_manipulations.go
+++ bit_manipulations.go
@@ -1,9 +1,41 @@
 package ctci
 
+import (
+	"errors"
+	"strings"
+)
+
+// Inserts M into N from indices i to j.
 func Insert(N, M int32, i, j uint) int32 {
 	mask := ((int32(1) << (j - i)) - 1) << i
 	result := N & (^mask)
 	result |= (M << i)
 
 	return result
+}
+
+// Return a binary representation of a float between 0 and 1 as a string.
+func BinaryString(x float32) (string, error) {
+	if x < 0.0 || x >= 1.0 {
+		return "ERROR", errors.New("x not in range 0 to 1")
+	}
+
+	var sb strings.Builder
+	sb.WriteRune('.')
+	var nextVal float32 = 1.0
+
+	for i := 0; i < 32; i++ {
+		nextVal /= 2.0
+		if x >= nextVal {
+			sb.WriteRune('1')
+			x -= nextVal
+			if x == 0.0 {
+				return sb.String(), nil
+			}
+		} else {
+			sb.WriteRune('0')
+		}
+	}
+
+	return sb.String(), errors.New("Could not represent in 32 bits")
 }
--- bit_manipulations_test.go
+++ bit_manipulations_test.go
@@ -16,3 +16,28 @@ func TestInsertion(t *testing.T) {
 		t.Error(output)
 	}
 }
+
+// Test converting a float to a binary string.
+func TestBinaryString(t *testing.T) {
+	var input float32 = 0.72
+	expected := ".1011100001010001111011"
+
+	output, err := BinaryString(input)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if output != expected {
+		t.Error(output)
+	}
+
+	output, err = BinaryString(1.0)
+	if err == nil {
+		t.Error(output)
+	}
+
+	output, err = BinaryString(-0.1)
+	if err == nil {
+		t.Error(output)
+	}
+}
