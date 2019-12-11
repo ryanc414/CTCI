c9d1b467de148a4971c2ac0862a4fe35854a4257
Ryan Collingham <ryanc118@gmail.com>
2019-12-11 16:23:05 +0000

    Implement simpler solution for flipbit

--- bit_manipulations.go
+++ bit_manipulations.go
@@ -47,101 +47,28 @@ func FlipBitToWin(x int32) int {
 		return 32
 	}
 
-	bits := intToBits(x)
-	groups := bitsToGroups(bits[:])
-    return mostBitsFromGroups(groups)
-}
+	currentLength := 0
+	prevLength := 0
 
-// Convert a 32-bit integer to an array of bools indicating the bits.
-func intToBits(x int32) [32]bool {
-	var bits [32]bool
+	// Can always flip at least one bit.
+	maxLength := 1
 
-	for i := 0; i < 32; i++ {
-		if x == 0 {
-			break
+	for x != 0 {
+		// Check if current bit is 1.
+		if x&1 == 1 {
+			currentLength++
+		} else {
+			if x&2 == 0 {
+				prevLength = 0
+			} else {
+				prevLength = currentLength
+			}
+			currentLength = 0
 		}
 
-		if x%2 == 1 {
-			bits[31-i] = true
-		}
-
+		maxLength = max(prevLength+currentLength+1, maxLength)
 		x >>= 1
 	}
 
-	return bits
-}
-
-// Convert a slice of bits to a slice containing the size of contiguous groups
-// of 0s and 1s.
-func bitsToGroups(bits []bool) []int {
-	var groups []int
-	currGroupSize := 0
-    currCounting := false  // start counting 0s
-
-	for i := range bits {
-		if bits[i] == currCounting {
-			currGroupSize++
-		} else {
-			groups = append(groups, currGroupSize)
-			currGroupSize = 1
-            currCounting = !currCounting
-		}
-	}
-
-	if currGroupSize > 0 {
-		groups = append(groups, currGroupSize)
-	}
-
-	return groups
-}
-
-// Return the largest size group of 1 bits that can be made by flipping
-// exactly one bit given the size of alternating groups of 0s and 1s.
-func mostBitsFromGroups(groups []int) int {
-    if len(groups) == 0 {
-        return 0
-    } else if len(groups) == 1 {
-        return 1
-    }
-
-    mostBits := 0
-
-    for i := 0; i < len(groups); i += 2 {
-        if groups[i] > 0 {
-            var numBits int
-
-            if groups[i] == 1 {
-                numBits = leftBits(groups, i) + rightBits(groups, i) + 1
-            } else {
-                numBits = max(leftBits(groups, i), rightBits(groups, i)) + 1
-            }
-
-            if numBits > mostBits {
-                mostBits = numBits
-            }
-        }
-    }
-
-    return mostBits
-}
-
-// Return the number of bits to the left of the current position, or 0 if at
-// the start.
-func leftBits(groups []int, i int) int {
-    if i > 0 {
-        return groups[i - 1]
-    } else {
-        return 0
-    }
-}
-
-
-// Return the number of bits to the right of the current position, or 0 if at
-// the end.
-func rightBits(groups []int, i int) int {
-    if i + 1 < len(groups) {
-        return groups[i + 1]
-    } else {
-        return 0
-    }
+	return maxLength
 }
