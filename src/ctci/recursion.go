ff802a7622d13051571f342b095520ede5a46e1c
Ryan Collingham <ryanc118@gmail.com>
2020-01-08 14:02:35 +0000

    Implement robot recursively

--- src/ctci/recursion.go
+++ src/ctci/recursion.go
@@ -1,5 +1,7 @@
 package ctci
 
+import "errors"
+
 // Calculate the number of ways of navigating a fixed number of steps in
 // jumps of 1, 2 and 3 only.
 func NumStepWays(numSteps int) int {
@@ -47,4 +49,79 @@ func NumStepWaysIter(numSteps int) int {
 	}
 
 	return z
+}
+
+// The robot's grid is made up of spaces it can and cannot visit.
+type RobotGrid [][]bool
+
+type RobotPath []GridDirection
+
+// The robot can only go right or down.
+var RobotDirections = []GridDirection{Right, Down}
+
+// Return a new grid for a robot of given size. Initially all squares are set
+// to true.
+func InitRobotGrid(size int) RobotGrid {
+	grid := make(RobotGrid, size)
+	for i := range grid {
+		grid[i] = make([]bool, size)
+		for j := range grid[i] {
+			grid[i][j] = true
+		}
+	}
+
+	return grid
+}
+
+// Finds the first valid path for the robot from top left to bottom right.
+func (grid RobotGrid) FindPath() (RobotPath, error) {
+	if len(grid) == 0 || len(grid[0]) == 0 {
+		return nil, errors.New("Empty grid")
+	}
+
+	if !grid[0][0] {
+		return nil, errors.New("Starting space is invalid.")
+	}
+
+	return grid.findPathRecur(
+		GridCoords{Row: 0, Col: 0},
+		make(RobotPath, 0, len(grid)),
+	)
+}
+
+// Recursive implementation.
+func (grid RobotGrid) findPathRecur(
+	currPos GridCoords, currPath RobotPath,
+) (RobotPath, error) {
+	// Base case: if we are at the end, return the current path.
+	if currPos.Row == len(grid)-1 && currPos.Col == len(grid[0])-1 {
+		return currPath, nil
+	}
+
+	// Try branching out right or down if possible.
+	for i := range RobotDirections {
+		newPos := currPos.MoveDirection(RobotDirections[i])
+		if grid.validCoords(newPos) && grid[newPos.Row][newPos.Col] {
+			newPath := append(currPath, RobotDirections[i])
+			fullPath, err := grid.findPathRecur(newPos, newPath)
+			if err == nil {
+				return fullPath, nil
+			}
+		}
+	}
+
+	// No valid path found, so return error.
+	return nil, errors.New("No valid path found.")
+}
+
+func (grid RobotGrid) validCoords(coords GridCoords) bool {
+	if coords.Row < 0 || coords.Row >= len(grid) {
+		return false
+	}
+
+	if coords.Col < 0 || coords.Col >= len(grid) {
+		return false
+	}
+
+	return true
 }
--- src/ctci/recursion_test.go
+++ src/ctci/recursion_test.go
@@ -14,3 +14,80 @@ func TestNumStepWays(t *testing.T) {
 		t.Error(res)
 	}
 }
+
+// Test finding valid paths through a grid for a robot.
+func TestRobotPaths(t *testing.T) {
+	grid := InitRobotGrid(5)
+
+	// First try with an empty grid (all paths valid). The first path found
+	// should be the one that goes right 4 times, then down 4 times.
+	expected := RobotPath{
+		Right,
+		Right,
+		Right,
+		Right,
+		Down,
+		Down,
+		Down,
+		Down,
+	}
+
+	path, err := grid.FindPath()
+	if err != nil {
+		t.Error(err)
+	}
+	if !compareRobotPaths(path, expected) {
+		t.Error(path)
+	}
+
+	// Now try a slightly more complex path for our robot.
+	grid = InitRobotGrid(7)
+
+	grid[1][1] = false
+	grid[1][2] = false
+	grid[1][3] = false
+	grid[2][0] = false
+	grid[2][5] = false
+	grid[3][5] = false
+	grid[3][6] = false
+	grid[4][6] = false
+	grid[5][1] = false
+	grid[6][5] = false
+
+	expected = RobotPath{
+		Right,
+		Right,
+		Right,
+		Right,
+		Down,
+		Down,
+		Down,
+		Down,
+		Right,
+		Down,
+		Right,
+		Down,
+	}
+
+	path, err = grid.FindPath()
+	if err != nil {
+		t.Error(err)
+	}
+	if !compareRobotPaths(path, expected) {
+		t.Error(path)
+	}
+}
+
+func compareRobotPaths(actual, expected RobotPath) bool {
+	if len(actual) != len(expected) {
+		return false
+	}
+
+	for i := range actual {
+		if actual[i] != expected[i] {
+			return false
+		}
+	}
+
+	return true
+}
