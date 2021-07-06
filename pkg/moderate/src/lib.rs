b98bc3dc420013e84e0fe3f8e33685b69ae3a00c
Ryan Collingham <ryan@tokencard.io>
2021-07-06 22:50:32 +0100

    implement english int solution

--- pkg/moderate/src/lib.rs
+++ pkg/moderate/src/lib.rs
@@ -58,6 +58,24 @@ mod tests {
         assert_eq!(number_max(44, -12), 44);
         assert_eq!(number_max(-99, -121), -99);
     }
+
+    #[test]
+    fn test_english_int() {
+        assert_eq!(english_int(0).unwrap().as_str(), "zero");
+        assert_eq!(english_int(7).unwrap().as_str(), "seven");
+        assert_eq!(english_int(13).unwrap().as_str(), "thirteen");
+        assert_eq!(english_int(42).unwrap().as_str(), "forty two");
+        assert_eq!(english_int(256).unwrap().as_str(), "two hundred fifty six");
+        assert_eq!(
+            english_int(1234).unwrap().as_str(),
+            "one thousand two hundred thirty four"
+        );
+        assert_eq!(english_int(1000000).unwrap().as_str(), "one million",);
+        assert_eq!(
+            english_int(3250000000).unwrap().as_str(),
+            "three billion two hundred fifty million"
+        );
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -241,4 +259,103 @@ fn smallest_diff_from(n: i64, b: &[i64]) -> Option<(i64, usize)> {
 pub fn number_max(x: i64, y: i64) -> i64 {
     let diff = (y - x).abs();
     (x + y + diff) / 2
+}
+
+const DIGITS: [&str; 10] = [
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+const TEENS: [&str; 10] = [
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+];
+
+const TENS: [&str; 8] = [
+    "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety",
+];
+
+const HUNDRED: &str = "hundred";
+
+const THOUSANDS: [&str; 4] = ["thousand", "million", "billion", "trillion"];
+
+const MAX: i64 = 10000000000000;
+
+pub fn english_int(x: i64) -> Result<String, ()> {
+    if x > MAX || x < 0 {
+        return Err(());
+    }
+
+    if x == 0 {
+        return Ok(String::from(DIGITS[0]));
+    }
+
+    let mut s = String::new();
+
+    for i in 0..4 {
+        let limit: i64 = 10i64.pow(3 * (4 - i));
+        if x < limit {
+            continue;
+        }
+
+        let base = (x / limit) % 1000;
+        if base == 0 {
+            continue;
+        }
+
+        let text = english_int_base(base).unwrap();
+        s.push_str(&text);
+        s.push(' ');
+        s.push_str(THOUSANDS[(4 - i - 1) as usize]);
+        s.push(' ');
+    }
+
+    let rest = x % 1000;
+    if rest > 0 {
+        let text = english_int_base(rest).unwrap();
+        s.push_str(&text);
+    }
+
+    s = s.trim_end().to_owned();
+
+    Ok(s)
+}
+
+fn english_int_base(x: i64) -> Result<String, ()> {
+    if x >= 1000 || x < 0 {
+        return Err(());
+    }
+
+    let mut s = String::new();
+
+    if x > 100 {
+        let hundreds = x / 100;
+        s.push_str(DIGITS[hundreds as usize]);
+        s.push(' ');
+        s.push_str(HUNDRED);
+        s.push(' ');
+    }
+
+    let tens = x % 100;
+    if tens >= 20 {
+        let i = tens / 10;
+        s.push_str(TENS[(i - 2) as usize]);
+        if tens % 10 > 0 {
+            s.push(' ');
+            s.push_str(DIGITS[(tens % 10) as usize]);
+        }
+    } else if tens >= 10 {
+        s.push_str(TEENS[(tens - 10) as usize]);
+    } else if tens > 0 {
+        s.push_str(DIGITS[tens as usize]);
+    }
+
+    Ok(s)
 }
